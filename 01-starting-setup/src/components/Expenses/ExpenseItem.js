9ac3afa102b989f9a56404fd11ad7fb2c3a9ff26
박정규 <srk99279@naver.com>
2022-08-29 14:57:12 +0900

    components rearrangement

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,18 @@
+import React from "react";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
+import Card from "../UI/Card";
 
 function ExpenseItem(props) {
   // must be 1 root element
   return (
-    <div className="expense-item">
-      <ExpenseDate date={props.date} />
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />{" "}
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2> {props.title} </h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
