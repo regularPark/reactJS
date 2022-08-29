bd690901b9974c872059f1cc48b90d09a48350d2
박정규 <srk99279@naver.com>
2022-08-29 15:40:47 +0900

    add 'useState'

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
+  // element, updatingElement
+  const [title, setTitle] = useState(props.title); // only this position
+
+  const clickHandler = () => {
+    setTitle("Updated");
+  };
+
   // must be 1 root element
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />{" "}
       <div className="expense-item__description">
-        <h2> {props.title} </h2>
+        <h2> {title} </h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
