ea8737ba83e888627c45eb746b6399c5b997c8bb
박정규 <srk99279@naver.com>
2022-08-30 19:12:02 +0900

    add Input form

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -24,9 +25,13 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log(expense);
+  };
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
---
+++ 01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+
+import "./ExpenseForm.css";
+
+const ExpenseForm = (props) => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+  //   const [userInput, setUserInput] = useState({
+  //     enteredTitle: "",
+  //     enteredAmount: "",
+  //     enteredDate: "",
+  //   });
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
+    // });
+    //     setUserInput((prevState) => {
+    //       return { ...prevState, enteredTitle: event.target.value };
+    //     }); // 이 방법이 항상 최신 상태를 갖고 있어 좀 더 안전
+  };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value,
+    // });
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value,
+    // });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault(); // submit 버튼 클릭시 새로고침 방지
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    props.onSaveExpenseData(expenseData);
+    // 부모인 NewExpense에 정의된 함수를 props으로 받아와 실행
+    // 양방향 바인딩. 스프레딩된 expenseData가 매개변수로 전달되는 것을 확인가능
+    // 부모 컴포넌트와의 통신
+
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate(""); // form에 남아있는 input 입력값 없애기
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            onChange={amountChangeHandler}
+            min="0.01"
+            step="0.01"
+            value={enteredAmount}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            value={enteredDate}
+            onChange={dateChangeHandler}
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
