efad56a4dd868c45d9d073d434a2dc22b42b4a89
JayPark0110 <90092440+JayPark0110@users.noreply.github.com>
2022-09-03 19:03:01 +0900

    section 2

---
+++ 01-starting-project/src/components/UI/Button/Button.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+import styles from "./Button.module.css";
+
+// import styled from "styled-components";
+
+// const Button = styled.button`
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
+
+//   @media (min-width: 768px) {
+//     width: auto;
+//   }
+
+//   &:focus {
+//     outline: none;
+//   }
+
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+// `;
+
+//css 모듈을 이용하여 해당 컴포넌트의 클래스명을 고유하게 정해줌 css파일을 고유하게 만듦.
+const Button = (props) => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
