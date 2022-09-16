40570ce5afc841fb4088954efc3cf1997323c2b4
박정규 <srk99279@naver.com>
2022-09-16 14:40:00 +0900

    custom-hook

--- more-realistic-custom/src/App.js
+++ more-realistic-custom/src/App.js
@@ -7,31 +7,30 @@ import useHttp from "./hooks/use-http";
 function App() {
   const [tasks, setTasks] = useState([]);
 
-  const transformTasks = (tasksObj) => {
-    const loadedTasks = [];
-
-    for (const taskKey in tasksObj) {
-      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
-    }
-
-    setTasks(loadedTasks);
-  };
-
-  const {
-    isLoading,
-    error,
-    sendRequest: fetchTasks,
-  } = useHttp(
-    {
-      url: "https://realistic-custom-hooks-default-rtdb.firebaseio.com/tasks.json",
-    },
-    transformTasks
-  );
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    const transformTasks = (tasksObj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    };
+
+    fetchTasks(
+      {
+        url: "https://realistic-custom-hooks-default-rtdb.firebaseio.com/tasks.json",
+      },
+      transformTasks
+    );
+  }, [fetchTasks]);
   // 무한 루프때문에 나중에 의존성 추가
+  // 함수는 객체이기 때문에 같은 로직을 가진 객체이더라도 함수가 재생성되면
+  // 이는 메모리에서 새로운 객체로 인식되어 재실행을 유발함.
+  // 이를 방지하려면 use-http에서 sendRequest를 useCallback으로 감싸야함.
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
