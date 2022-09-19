8c202ebef3191772dfd7efc8d11e9dfe76cbd536
박정규 <srk99279@naver.com>
2022-09-19 20:12:54 +0900

    custom-hook-v1

---
+++ form/src/components/SimpleInput.js
@@ -0,0 +1,95 @@
+import { useState } from "react";
+
+import useInput from "../hooks/use-input";
+
+const SimpleInput = (props) => {
+  const {
+    value: enteredName,
+    isValid: enteredNameIsValid,
+    hasError: nameInputHasError,
+    valueChangeHandler: nameChangedHandler,
+    inputBlurHandler: nameBlurHandler,
+    reset: resetNameInput,
+  } = useInput((value) => value.trim() !== "");
+
+  const {
+    value: enteredEmail,
+    isValid: enteredEmailIsValid,
+    hasError: emailInputHasError,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmailInput,
+  } = useInput((value) => value.includes("@"));
+
+  let formIsValid = false;
+
+  if (enteredNameIsValid && enteredEmailIsValid) {
+    formIsValid = true;
+  }
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    if (!enteredNameIsValid) {
+      return;
+    }
+
+    resetNameInput();
+
+    resetEmailInput();
+
+    // nameInputRef.current.value = ''; => NOT IDEAL,  Don't manipulate the DOM
+  };
+
+  /* value가 폼이 제출될때 한번만 필요하다면 모든 키 입력마다
+  *  상태 값을 업데이트하는 것이 불필요하므로
+   ref가 낫다. + 입력값을 초기화하고 싶을 때
+  *
+  * 반면에, 즉각적인 유효성 검증을 위해서는 입력 값이 매번 필요하므로
+  * State를 사용하는 것이 낫다.
+  */
+
+  const nameInputClasses = nameInputHasError
+    ? "form-control invalid"
+    : "form-control";
+
+  const emailInputClasses = emailInputHasError
+    ? "form-control invalid"
+    : "form-control";
+
+  return (
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          onChange={nameChangedHandler}
+          onBlur={nameBlurHandler}
+          value={enteredName}
+        />
+        {nameInputHasError && (
+          <p className="error-text">Name must not be empty.</p>
+        )}
+      </div>
+      <div className={emailInputClasses}>
+        <label htmlFor="email">Your E-Mail</label>
+        <input
+          type="email"
+          id="email"
+          onChange={emailChangeHandler}
+          onBlur={emailBlurHandler}
+          value={enteredEmail}
+        />
+        {emailInputHasError && (
+          <p className="error-text">Please enter a valid email.</p>
+        )}
+      </div>
+      <div className="form-actions">
+        <button disabled={!formIsValid}>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
