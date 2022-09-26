362fe3fa8a8918777ec857cc3b818d6cbabfdac9
박정규 <srk99279@naver.com>
2022-09-26 15:54:20 +0900

    front to back, back to front

--- redux-advanced/src/store/cart-slice.js
+++ redux-advanced/src/store/cart-slice.js
@@ -5,12 +5,19 @@ const cartSlice = createSlice({
   initialState: {
     items: [],
     totalQuantity: 0,
+    changed: false,
   },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
+
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
+      state.changed = true;
       if (!existingItem) {
         state.items.push({
           id: newItem.id,
@@ -29,6 +36,7 @@ const cartSlice = createSlice({
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
       state.totalQuantity--;
+      state.changed = true;
       if (existingItem.quantity === 1) {
         state.items = state.items.filter((item) => item.id !== id);
       } else {
@@ -38,6 +46,8 @@ const cartSlice = createSlice({
     },
   },
 });
+
+// 외부 정의
 
 export const cartActions = cartSlice.actions;
 
