869bdf31811b81f9de1bbe8dd0b6be4a8d5d43b4
박정규 <srk99279@naver.com>
2022-10-09 20:41:53 +0900

    TransitionGroup with React Transition Group

--- anim-01/src/components/Modal/Modal.js
+++ anim-01/src/components/Modal/Modal.js
@@ -11,31 +11,25 @@ const animationTiming = {
 
 const modal = (props) => {
   return (
-    <Transition
+    <CSSTransition
       mountOnEnter
       unmountOnExit
       in={props.show}
       timeout={animationTiming}
-    >
-      {(state) => {
-        const cssClasses = [
-          "Modal",
-          state === "entering"
-            ? "ModalOpen"
-            : state === "exiting"
-            ? "ModalClosed"
-            : null,
-        ];
-        return (
-          <div className={cssClasses.join(" ")}>
-            <h1>A Modal</h1>
-            <button className="Button" onClick={props.closed}>
-              Dismiss
-            </button>
-          </div>
-        );
+      classNames={{
+        enter: "",
+        enterActive: "ModalOpen",
+        exit: "",
+        exitActive: "ModalClosed",
       }}
-    </Transition>
+    >
+      <div className="Modal">
+        <h1>A Modal</h1>
+        <button className="Button" onClick={props.closed}>
+          Dismiss
+        </button>
+      </div>
+    </CSSTransition>
   );
 };
 
