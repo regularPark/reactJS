c11a5fddae49aaa343e75d15111446546c31b539
박정규 <srk99279@naver.com>
2022-10-09 19:59:03 +0900

    react animation

---
+++ anim-01/src/components/Modal/Modal.js
@@ -0,0 +1,42 @@
+import React from "react";
+import CSSTransition from "react-transition-group/CSSTransition";
+
+import "./Modal.css";
+import Transition from "react-transition-group/Transition";
+
+const animationTiming = {
+  enter: 400,
+  exit: 1000,
+};
+
+const modal = (props) => {
+  return (
+    <Transition
+      mountOnEnter
+      unmountOnExit
+      in={props.show}
+      timeout={animationTiming}
+    >
+      {(state) => {
+        const cssClasses = [
+          "Modal",
+          state === "entering"
+            ? "ModalOpen"
+            : state === "exiting"
+            ? "ModalClosed"
+            : null,
+        ];
+        return (
+          <div className={cssClasses.join(" ")}>
+            <h1>A Modal</h1>
+            <button className="Button" onClick={props.closed}>
+              Dismiss
+            </button>
+          </div>
+        );
+      }}
+    </Transition>
+  );
+};
+
+export default modal;
