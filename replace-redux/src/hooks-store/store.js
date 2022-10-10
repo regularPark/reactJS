9b846d7af53f03673d466fe801487363bffbadd8
박정규 <srk99279@naver.com>
2022-10-10 21:13:11 +0900

    using custom hook instead Redux

--- replace-redux/src/hooks-store/store.js
+++ replace-redux/src/hooks-store/store.js
@@ -1,8 +1,41 @@
 // state를 전역적으로 관리하는 솔루션(리액트와 js만으로)
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 let globalState = {};
-let listenters = [];
+let listeners = [];
 let actions = {};
 
-const useStore = () => {};
+export const useStore = (shouldListen = true) => {
+  const setState = useState(globalState)[1]; // update하는 함수 [1]
+
+  const dispatch = (actionIdentifier, payload) => {
+    const newState = actions[actionIdentifier](globalState, payload);
+    globalState = { ...globalState, ...newState };
+
+    for (const listener of listeners) {
+      listener(globalState);
+    }
+  };
+
+  useEffect(() => {
+    if (shouldListen) {
+      listeners.push(setState);
+    }
+
+    return () => {
+      // listeners 제거
+      if (shouldListen) {
+        listeners = listeners.filter((li) => li !== setState);
+      }
+    };
+  }, [setState, shouldListen]);
+
+  return [globalState, dispatch];
+};
+
+export const initStore = (userActions, initialState) => {
+  if (initialState) {
+    globalState = { ...globalState, ...initialState };
+  }
+  actions = { ...actions, ...userActions };
+};
