7a644793c5730f47b90f32162ac9013dbca01a30
박정규 <srk99279@naver.com>
2022-10-14 19:35:46 +0900

    hooks-detail

---
+++ react-hooks/src/components/Ingredients/IngredientForm.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+import Card from "../UI/Card";
+import "./IngredientForm.css";
+
+/** useState는 다른 함수에 내부에서 선언될 수 없다.
+ * 컴포넌트의 루트에서 사용되어야 함.
+ * 또한 조건문에서도 훅을 사용할 수 없음
+ */
+const IngredientForm = React.memo((props) => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    // ...
+    props.onAddIngredient({ title: enteredTitle, amount: enteredAmount });
+  };
+
+  return (
+    <section className="ingredient-form">
+      <Card>
+        <form onSubmit={submitHandler}>
+          <div className="form-control">
+            <label htmlFor="title">Name</label>
+            <input
+              type="text"
+              id="title"
+              value={enteredTitle}
+              onChange={(event) => {
+                setEnteredTitle(event.target.value);
+              }}
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="amount">Amount</label>
+            <input
+              type="number"
+              id="amount"
+              value={enteredAmount}
+              onChange={(event) => {
+                setEnteredAmount(event.target.value);
+              }}
+            />
+          </div>
+          <div className="ingredient-form__actions">
+            <button type="submit">Add Ingredient</button>
+          </div>
+        </form>
+      </Card>
+    </section>
+  );
+});
+
+export default IngredientForm;
